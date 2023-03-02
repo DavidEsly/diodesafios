2437d2e729cb311138ff42782c7fdfd6bab099d2
DavidEsly <david.esly@hotmail.com>
2023-03-02 16:04:03 -0300

    Renomeado a pasta e adicionado echo ao script

--- Desafio01/script01.sh
+++ Desafio01/script01.sh
@@ -1,5 +1,6 @@
 # /bin/bash
 
+echo "Removendo usuários caso existam"
 userdel -r -f carlos
 userdel -r -f  maria
 userdel -r -f  joao
@@ -9,23 +10,23 @@ userdel -r -f  roberto
 userdel -r -f  josefina
 userdel -r -f  amanda
 userdel -r -f  rogerio
-
+echo "Acessando diretório raiz"
 cd /
-
+echo "Criando pastas"
 mkdir publico adm ven sec
-
+echo "Criando grupos"
 groupadd GRP_ADM 
 groupadd GRP_VEN 
 groupadd GRP_SEC
-
+echo "Definindo proprietário e grupo de cada pasta"
 chown root:GRP_ADM adm
 chown root:GRP_VEN ven
 chown root:GRP_SEC sec
-
+echo "Liberando acesso total a pasta publico"
 chmod 777 publico
-
+echo "Restrigindo o acesso"
 chmod 770 adm ven sec
-
+echo "Criando usuários"
 useradd carlos -c "Carlos Silva" -s /bin/bash -m -p $(openssl passwd -5 123@mudar) -G GRP_ADM
 useradd maria -c "Maria Silva" -s /bin/bash -m -p $(openssl passwd -5 123@mudar) -G GRP_ADM
 useradd joao -c "Joao Silva" -s /bin/bash -m -p $(openssl passwd -5 123@mudar) -G GRP_ADM
